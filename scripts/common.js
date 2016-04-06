cf073e59d6673acb8f26f265ef1e915fc0c9f2f7
jianyinniu <714074509@qq.com>
2016-04-06 16:53:40 +0800

    0406 页面调整

--- scripts/common.js
+++ scripts/common.js
@@ -43,3 +43,29 @@ doT.templateSettings = {
   interpolate: /\<\%=([\s\S]+?)\%\>/g,
   varname: 'it'
 };
+
+
+
+util.windowOpen = function(url,target){
+  var a = document.createElement("a");
+  a.setAttribute("href", url);
+  if(target == null){
+    target = '_blank';
+  }
+  a.setAttribute("target", target);
+  document.body.appendChild(a);
+  if(a.click){
+    a.click();
+  }else{
+    try{
+      var evt = document.createEvent('Event');
+      a.initEvent('click', true, true);
+      a.dispatchEvent(evt);
+    }catch(e){
+      window.open(url);
+    }
+  }
+  document.body.removeChild(a);
+}
+// util.windowOpen(url,'_blank');
+
